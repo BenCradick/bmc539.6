dc45c7df14755f21677325ba401328c6c366aaac
Ben Cradick <ben.cradick@gmail.com>
2018-04-25 09:26:19 -0500

    started building the linked list Q's

--- makefile
+++ makefile
@@ -6,6 +6,7 @@ $(TARGET): $(OBJS)
 main.o: sim_checkout.c
 	$(CC) -c sim_checkout.c
 
-
+debug:
+	$(CC)  sim_checkout.c -DDEBUG -std=c99 -o $(TARGET)
 clean:
 	/bin/rm -f *.o* $(TARGET)
--- sim_checkout.c
+++ sim_checkout.c
@@ -4,25 +4,31 @@
 #include <stdlib.h>
 
 #ifndef DEBUG
-//#include "GenerateCustomers/generate_customers.h"
+#include "GenerateCustomers/generate_customers.h"
 #endif
-
+//time required to be serviced, time of arrival, time at front of line.
 typedef struct{
-    int t_queue;
-    int t_enqueue;
-    int t_dequeue;
+    int t_service, t_enqueue, t_service_start;
+
+    struct Customer *next;
 
 }Customer;
-//Overall queue structure: the current customer.
+//Overall queue structure: has a variable attached to the list that gives time spent idle, and the size of the list.
 struct Queue{
-    Customer current;
-    Customer* next;
+    
+    int t_idle, size;
+    struct Customer *front, *back;
 
 };
+//Q is short for Queue
+struct Queue* generateQ();
+struct Customer* newCustomer(int t_service, int t_enqueue);
 
 int main(){
     FILE *fp;
     fp = fopen("GenerateCustomers/customers", "rb");
+    int num_customers = 0;
+
 
     fseek(fp, 0L, SEEK_END);
     const int size = ftell(fp);
@@ -48,5 +54,12 @@ int main(){
 
     return 0;
 }
-
+struct Queue* generateQ(){
+    struct Queue *queue = (struct Queue *)malloc(sizeof(struct Queue));
+    
+}
+struct Customer *newCustomer(int t_service, int t_enqueue)
+{
+    //Customer
+}
 //TODO function to assign values to customers needs to iterate through array and get the hex in the right order.
