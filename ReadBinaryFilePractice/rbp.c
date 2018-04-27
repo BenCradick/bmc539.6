8e2312fda58a7e15d9fb3e4977c267e2e3336366
Ben Cradick <ben.cradick@gmail.com>
2018-04-27 09:39:28 -0500

    appended get_whence to take you straight to the record number

--- ReadBinaryFilePractice/rbp.c
+++ ReadBinaryFilePractice/rbp.c
@@ -11,20 +11,16 @@ int get_whence(int);
 int main(){
     FILE* fp;
     fopen("acct_info", "rb");
-    int a = get_whence(5);
-    printf("%d\n", a);
-    //printf("%d\n", sizeof(acct_info_t));
-    //printf("4(sizeof(char[40]): %d + sizeof(int): %d + sizeof(float): %d) + 2(sizeof(unsigned int)) = %d\n", sizeof(char[40]), sizeof(int), sizeof(float), sizeof(unsigned int), a);
+    fseek(fp,)
     return 0;
 }
 
 int get_whence(int record){
     int bytes = 0;
-    printf("%d\n",bytes);
     bytes += 2*sizeof(unsigned int);
-    printf("%d\n", bytes);
     bytes += record*sizeof(acct_info_t);
-    printf("%d\n", bytes);
+    bytes += sizeof(char[40])
+    bytes += sizeof(int);
     return bytes; 
 
 }
